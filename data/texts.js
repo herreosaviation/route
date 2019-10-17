e5c89c267985430296e0a4657852f1bfd04a91d2
jan <jan.herr@outlook.com>
2019-10-17 12:04:58 +0200

    update

--- data/texts.js
+++ data/texts.js
@@ -102,7 +102,7 @@ define(["require", "exports"], function (require, exports) {
                 else {
                     if (isde) {
                         if (index == 1) {
-                            return "Es ist voraussichtlich ein Taknstop erforderlich. Für den Tankstop müssen ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
+                            return "Es ist voraussichtlich ein Tankstop erforderlich. Für den Tankstop müssen ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
                         }
                         else {
                             return "Es müssen vorraussichtlich " + index.toString() + " Tankstops durchgeführt werden. Für die Tankstops müssen jeweils ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
--- texts.ts
+++ texts.ts
@@ -98,7 +98,7 @@ export function getText(type: Texts, index: number = null): string {
             else {
                 if (isde) {
                     if (index == 1) {
-                        return "Es ist voraussichtlich ein Taknstop erforderlich. Für den Tankstop müssen ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
+                        return "Es ist voraussichtlich ein Tankstop erforderlich. Für den Tankstop müssen ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
                     }
                     else {
                         return "Es müssen vorraussichtlich " + index.toString() + " Tankstops durchgeführt werden. Für die Tankstops müssen jeweils ca. 45 Minuten eingeplant werden. Dies ist in der Reisezeit inbegriffen.";
